5141d39e3239bf5ed03cfd1f06f62164bda89153
Mariana <marianaufc@gmail.com>
2021-08-22 17:19:56 -0300

    Added move to repository list page and list

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
 export default function Home() {
     const [user, setUser] = useState('');
+    const history = useHistory();
+    
     function handleSearch() {
         axios.get(`https://api.github.com/users/${user}/repos`).then(response => {
             const repNames = response.data.map(rep => rep.name);
             localStorage.setItem('repNames', JSON.stringify(repNames));
+            history.push('./repositories');
         });
     }
 
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Repositories() {
+    const [ repositories, setRepositories ] = useState([]);
+    useEffect(() => {
+        const repNames = JSON.parse(localStorage.getItem('repNames'));
+        setRepositories(repNames);
+        localStorage.clear();
+    }, []);  
+     
     return(
-        <h1>Repository List</h1>
+        <>
+            <h1>Repository List</h1>
+            <ul>
+                {
+                    repositories.map(repository => {
+                        return (
+                            <li>{repository}</li>
+                        )
+                        }
+                    )
+                }
+            </ul>
+        </>
     );
 }
